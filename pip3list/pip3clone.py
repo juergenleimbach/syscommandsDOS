6eb22b804acccef30e69e1bf7d47ae300733da5d
juergenleimbach <jleimbach@live.de>
2018-06-12 14:15:32 +0200

    ear

---
+++ pip3list/pip3clone.py
@@ -0,0 +1,27 @@
+# Template fuer befehle mit cmd-Befehlsinterpreter
+# er hier gesetzte Befehl wird von cmd ausgeführt,
+# erzeugteliste ist ein Array mit allen Bildschirmzeilen
+# und kann zum Debuggen oder der weiterverarbeitung der Werte
+# benutzt werden
+from rundos import runcmd, printdosscreen
+import csv
+
+befehl = 'python.exe -m pip install --upgrade pip'
+fuehraus = runcmd (befehl)
+erzeugteliste = printdosscreen(fuehraus)
+with open('pip3data.txt') as inf:
+    reader = csv.reader(inf, delimiter=" ")
+    
+    col = list(zip(*reader))[0]
+print (col)
+
+
+
+
+num_lines = sum(1 for line in open('pip3data.txt'))
+for i in range(0,num_lines):
+    print(col[i])
+
+for werte in range(0,len(erzeugteliste)):
+    print (erzeugteliste[werte])
+
--- pip3list/pip3list.py
+++ pip3list/pip3list.py
@@ -1,16 +1,14 @@
-# Template fuer befehle mit cmd-Befehlsinterpreter
-# er hier gesetzte Befehl wird von cmd ausgeführt,
-# erzeugteliste ist ein Array mit allen Bildschirmzeilen
-# und kann zum Debuggen oder der weiterverarbeitung der Werte
-# benutzt werden
+"""
+Ganz einfaches Tool zum sichern der installierten pip3 Pakete
+in einer vorhandenen python installation
+"""
 from rundos import runcmd, printdosscreen
 
 befehl = 'pip3 list'
 fuehraus = runcmd (befehl)
 erzeugteliste = printdosscreen(fuehraus)
-
-# Debugging / Weiterverwertung
-# des Bildschirminhalts in weiteren Programmteilen
-# auskommentieren für eine lesbare Liste
-for werte in range(0,len(erzeugteliste)):
-    print (erzeugteliste[werte])
+datei = open("pip3data.txt","w+")
+for werte in range(2,len(erzeugteliste)):
+    erzeugteliste[werte] = " ".join(erzeugteliste[werte].split())
+    datei.write (erzeugteliste[werte]+'\n')
+datei.close()
